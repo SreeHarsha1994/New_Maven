57d0bcd354b7702cbb1f6ea7884910044901ea3d
SREEHARSH <SREEHARSH@LAPTOP-SO7U3JNL>
2022-01-06 20:30:06 +0530

    New

---
+++ src/test/java/com/crm_MavenNew/Smoke.java
@@ -0,0 +1,12 @@
+package com.crm_MavenNew;
+
+import org.testng.annotations.Test;
+
+public class Smoke {
+	   @Test
+	   public void demo() {
+		   System.out.println("new smoke");
+	   }
+
+
+}
---
+++ src/test/java/com/crm_MavenNew/Test_Case.java
@@ -0,0 +1,12 @@
+package com.crm_MavenNew;
+
+import org.testng.annotations.Test;
+
+public class Test_Case {
+	
+   @Test
+   public void demo() {
+	   System.out.println("new Commit");
+   }
+
+}
